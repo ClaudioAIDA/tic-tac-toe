f4dc1d585e2a3495790630c7fa7491804fd74580
Claudio López Ardura <cllopez@aidacanarias.com>
2021-12-03 09:51:36 +0100

    Create test for adding tokens

--- src/GameEngine.ts
+++ src/GameEngine.ts
@@ -1,4 +1,7 @@
 export class GameEngine{
+    AddTokenToPosition(x: any, y: any) {
+        throw new Error("Method not implemented.");
+    }
 
     constructor() { }
 
--- test/GameEngine.spec.ts
+++ test/GameEngine.spec.ts
@@ -6,4 +6,13 @@ describe('GameEngine should', () => {
         var game = new GameEngine();
         expect(game.render()).toBe(expectedCanvas);
     })
+
+    test('add the token of the current player when a position is provided', () => {
+        var expectedCanvas = "|X  |   |   |"
+
+        var game = new GameEngine();
+
+        game.AddTokenToPosition(0,0)
+        expect(game.render()).toBe(expectedCanvas);
+    })
 })
